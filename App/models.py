8c012996dfaacfa063524cc82dabd59e02ecd823
andanlove <40208203+andanlove@users.noreply.github.com>
2018-10-15 20:32:00 +0800

    Merge branch 'master' of https://github.com/flaskxiangmu/taopiaopiao into Version1
    # Conflicts:
    #	App/models.py

--- App/models.py
+++ App/models.py
@@ -1,8 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
 
-<<<<<<< HEAD
-db = SQLAlchemy()
-=======
 db = SQLAlchemy()
 
 
@@ -16,4 +13,4 @@ class Cat(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(64))
     age = db.Column(db.Integer)
->>>>>>> 4d6f1f5d5571edb0c642df182c338c96d9c2a4b5
+
