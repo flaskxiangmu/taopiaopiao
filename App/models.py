f1de49472d7c6abb2ff5516dc48c817412a61251
andanlove <40208203+andanlove@users.noreply.github.com>
2018-10-15 20:27:40 +0800

    '烦烦烦'

--- App/models.py
+++ App/models.py
@@ -1,3 +1,19 @@
 from flask_sqlalchemy import SQLAlchemy
 
+<<<<<<< HEAD
 db = SQLAlchemy()
+=======
+db = SQLAlchemy()
+
+
+class Hello(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(64))
+    age = db.Column(db.Integer)
+
+
+class Cat(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(64))
+    age = db.Column(db.Integer)
+>>>>>>> 4d6f1f5d5571edb0c642df182c338c96d9c2a4b5
