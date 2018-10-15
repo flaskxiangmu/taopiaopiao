161f4ce4036e6364b34c8ca395f466c58b0aa385
Yuandi <175047456@qq.com>
2018-10-15 21:56:38 +0800

    This is Yuandi's models

--- App/models.py
+++ App/models.py
@@ -13,3 +13,6 @@ class Cat(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(64))
     age = db.Column(db.Integer)
+
+
+# This is Yuandi's models
